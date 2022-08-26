048dc4a1ec5b49d39c281fcc314efa0baf1a7b62
solopvova <solopvova1@gmail.com>
2022-08-26 21:32:28 +0800

    a bit more validations

--- domain/service/helper.js
+++ domain/service/helper.js
@@ -2,21 +2,31 @@ const { Message } = require("../model/Message");
 const { Room } = require("../model/Room");
 
 const joinChat = (participant, chat) => {
+  if (chat.hasParticipant(participant)) {
+    throw new Error("Already participant");
+  }
   participant.addChat(chat);
   chat.addParticipant(participant);
 };
 
 const leaveChat = (participant, chat) => {
+  if (!chat.hasParticipant(participant)) {
+    throw new Error("Not a participant");
+  }
   participant.removeChat(chat);
   chat.removeParticipant(participant);
 };
 
 const sendMessage = (participant, chat, text) => {
+  if (!chat.hasParticipant(participant)) {
+    throw new Error("Only participants can send a message");
+  }
   const message = new Message(participant, text);
   chat.addMessage(message);
 };
 
 const createDirectChat = (p1, p2) => {
+  // todo: check if exist
   const room = new Room(null);
   room.addParticipant(p1);
   room.addParticipant(p2);
--- domain/tests/index.js
+++ domain/tests/index.js
@@ -2,41 +2,34 @@ const { stats, strictEqual, throws, offTests } = require("./util");
 // offTests();
 const { Participant } = require("../model/Participant");
 const { Room } = require("../model/Room");
-const {
-  joinChat,
-  sendMessage,
-  createDirectChat,
-  leaveChat,
-} = require("../service/helper");
+const helper = require("../service/helper");
 
 const elon = new Participant("Elon M.");
 const steve = new Participant("Steve J.");
+const mark = new Participant("Mark Z.");
+
 const startups = new Room("Startups");
 const tesla = new Room("Tesla");
 const spaceX = new Room("SpaceX");
 
-joinChat(elon, startups);
+helper.joinChat(elon, startups);
 strictEqual(elon.hasChat(startups), true);
-
-joinChat(steve, startups);
-
+helper.joinChat(steve, startups);
 strictEqual(startups.participants.length, 2);
-sendMessage(elon, startups, "Hi, there!");
-sendMessage(steve, startups, "Hello!");
+helper.sendMessage(elon, startups, "Hi, there!");
+helper.sendMessage(steve, startups, "Hello!");
+throws(() => {
+  helper.sendMessage(mark, startups, "Hola!");
+});
 strictEqual(startups.messages.length, 2);
-
-joinChat(elon, tesla);
-joinChat(elon, spaceX);
+helper.joinChat(elon, tesla);
+helper.joinChat(elon, spaceX);
 strictEqual(elon.chats.length, 3);
-
-leaveChat(elon, tesla);
+helper.leaveChat(elon, tesla);
 strictEqual(tesla.hasParticipant(elon), false);
 strictEqual(elon.hasChat(tesla), false);
-
-const elonSteveDirectConversation = createDirectChat(elon, steve);
-
+const elonSteveDirectConversation = helper.createDirectChat(elon, steve);
 throws(() => {
-  const mark = new Participant("Mark Z.");
   elonSteveDirectConversation.addParticipant(mark);
 });
 

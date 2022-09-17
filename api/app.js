09da51f2a894c47ca8f01055e8be677104a32e47
solopvova <solopvova1@gmail.com>
2022-09-17 10:19:54 +0800

    init sql

--- api/app.js
+++ api/app.js
@@ -6,13 +6,11 @@ dotenv.config();
 
 const { logRequest } = require("../domain/tests/util");
 const { Controller } = require("./controller");
-const { StorageInMemory } = require("../domain/service/storage");
-// const { StorageInPostgreSql } = require("../domain/service/storagePg");
+const { StorageInPostgreSql } = require("../domain/service/storagePg");
 
 dotenv.config();
 app.use(bodyParser.json());
 app.use(logRequest);
 
-new Controller(app, new StorageInMemory());
-// new Controller(app, new StorageInPostgreSql());
+new Controller(app, new StorageInPostgreSql());
 app.listen(8090, () => console.log("app is running on port " + 8090));
---
+++ domain/model/migrations/init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE room (
+    id SERIAL PRIMARY KEY,
+    name text,
+    is_direct boolean
+);
+
+CREATE TABLE participant (
+    id SERIAL PRIMARY KEY,
+    name text
+);
+
+CREATE TABLE message (
+    id SERIAL PRIMARY KEY,
+    room integer REFERENCES room(id),
+    author integer REFERENCES participant(id),
+    time date
+);
